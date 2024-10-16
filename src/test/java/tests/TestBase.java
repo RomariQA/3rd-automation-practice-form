580388d48988c463a52580f0cd1632fe04875e0e
Maya Romanovich <mariaromanovich@yandex.ru>
2024-10-16 12:18:14 +0300

    added configs

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,10 +14,13 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        Configuration.remote = "https://" + System.getProperty("login") + "@" +System.getProperty("remote");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
