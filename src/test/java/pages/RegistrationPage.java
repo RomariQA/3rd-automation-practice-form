5a28523d0024102ff659ff3f8873f933b6b351aa
Maya Romanovich <mariaromanovich@yandex.ru>
2024-10-16 14:58:38 +0300

    added fixes

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 
-import javax.swing.*;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
